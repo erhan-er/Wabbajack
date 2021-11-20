a76b6407cb7c4678d5e0c796aad25c12831af1dc
OsmanSemihTiryaki <semih.tiryaki@ug.bilkent.edu.tr>
2021-11-20 21:48:12 +0300

    Some entity classes added

---
+++ wabbajack_app/API/Entities/Place.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Place
+    {
+        public string Building { get; set; }  
+        public string Room { get; set; }
+    }
+}
