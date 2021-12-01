76d885003cc84e86f7bfe0e3b5f004ec3bda2886
Arda Serim <arda.serim@ug.bilkent.edu.tr>
2021-12-01 15:28:44 +0300

    Login Page fixed and SeeAllStudent page added

--- wabbajack_app/wabbajack_app/src/App.js
+++ wabbajack_app/wabbajack_app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import LoginPage from "./Pages/LoginPage";
 import Home from "./Pages/Home";
+import SeeAllStudents from './Pages/SeeAllStudents';
 import AppBar from "./Components/AppBar";
 import TestPage from "./Pages/TestPage";
 import { Outlet } from "react-router-dom";
@@ -11,12 +12,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route exact path = "/" element = {<LoginPage />}/>
-            <Route path = "/Home" element = {<Home />} />
-          </Routes>
-        </div>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/See-All-Students" element={<SeeAllStudents />} />
+        </Routes>
+      </div>
     </BrowserRouter>
 
   );
