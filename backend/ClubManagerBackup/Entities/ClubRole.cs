ab69b2cd9a8f687f6c3ad696debd1c94e9f0e767
Murat Furkan Uğurlu <furkan.ugurlu@ug.bilkent.edu.tr>
2021-12-14 14:34:51 +0300

    backend updated

---
+++ backend/ClubManagerBackup/Entities/ClubRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClubManagerBackup.Entities
+{
+    public class ClubRole
+    {
+        public int ID { get; set; }
+        public string ClubRoleName { get; set; }
+    }
+}
